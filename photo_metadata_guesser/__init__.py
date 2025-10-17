76c84ab19cb39c3b20405c58e9a58a09a14c8b6f
Mathias Aleksander Emberland Østby <maostby@student.matnat.uio.no>
2025-10-17 14:51:00 +0200

    Add initial package structure and versioning information

---
+++ photo_metadata_guesser/__init__.py
@@ -0,0 +1,14 @@
+"""Photo Metadata Mapper/Guesser package.
+
+Utilities to map image EXIF GPS coordinates to timeline points and find
+closest matching locations by date.
+"""
+
+# __all__ = [
+#     "cli",
+#     "timeline_parser",
+#     "exif_reader",
+#     "mapper",
+# ]
+
+__version__ = "0.1.0"
